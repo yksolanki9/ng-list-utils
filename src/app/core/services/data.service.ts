fe14aaae6111602f26a357e058039bf9b6a457ca
yksolanki9 <yashsolanki1709@gmail.com>
2023-07-28 20:46:30 +0530

    Get data from json file and show the data in the card

---
+++ src/app/core/services/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CardDetails } from '../models/card-details.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<CardDetails[]> {
+    return this.http.get<CardDetails[]>('assets/data/data.json');
+  } 
+}
--- src/app/shared/components/card-grid/card-grid.component.ts
+++ src/app/shared/components/card-grid/card-grid.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CardDetails } from 'src/app/core/models/card-details.model';
+import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
   selector: 'app-card-grid',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-grid.component.scss']
 })
 export class CardGridComponent {
+  dataSource: CardDetails[];
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getData().subscribe((data) => {
+      //Slice the data to only show 6 cards
+      this.dataSource = data.slice(0, 6);
+    })
+  }
 }
