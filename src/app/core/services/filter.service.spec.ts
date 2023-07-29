7054378369bb07bfc64b6a9ff5679c75aca42d20
yksolanki9 <yashsolanki1709@gmail.com>
2023-07-29 20:35:16 +0530

    Add unit tests for filter service

--- src/app/core/services/filter.service.spec.ts
+++ src/app/core/services/filter.service.spec.ts
@@ -1,8 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FilterService } from './filter.service';
+import {
+  cardDetails1,
+  cardDetails2,
+  cardDetailsSortedByDate,
+  cardDetailsSortedByName,
+} from '../test-data/card-details.data';
 
-describe('FilterService', () => {
+//Should run tests only for this page when running `ng test`
+fdescribe('FilterService', () => {
   let service: FilterService;
 
   beforeEach(() => {
@@ -12,5 +19,81 @@ describe('FilterService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('search(): ', () => {
+    it('should return the correct result for exact match', () => {
+      const searchQuery = '"the king"';
+      const result = service.search(cardDetails1, searchQuery);
+      expect(result).toEqual([cardDetails1[0], cardDetails1[3]]);
+    });
+
+    it('should return the correct result for non-exact match', () => {
+      const searchQuery = 'the king';
+      const result = service.search(cardDetails1, searchQuery);
+      expect(result).toEqual(cardDetails1.slice(0, 4));
+    });
+
+    it('should return all entries for empty search string', () => {
+      const searchQuery = '""';
+      const result = service.search(cardDetails1, searchQuery);
+      expect(result).toEqual(cardDetails1);
+    });
+
+    it('should return empty array if no match found in name and description', () => {
+      const searchQuery = 'picsum';
+      const result = service.search(cardDetails1, searchQuery);
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('sort(): ', () => {
+    it('should correctly sort by name in ascending order', () => {
+      const sortString = 'name-asc';
+      const result = service.sort(cardDetails2, sortString);
+      expect(result).toEqual(cardDetailsSortedByName);
+    });
+
+    it('should correctly sort by name in descending order', () => {
+      const sortString = 'name-desc';
+      const result = service.sort(cardDetails2, sortString);
+      const cardDetailsCopy = [...cardDetailsSortedByName];
+      expect(result).toEqual(cardDetailsCopy.reverse());
+    });
+
+    it('should correctly sort by dateLastEdited in ascending order', () => {
+      const sortString = 'dateLastEdited-asc';
+      const result = service.sort(cardDetails2, sortString);
+      expect(result).toEqual(cardDetailsSortedByDate);
+    });
+
+    it('should correctly sort by dateLastEdited in descending order', () => {
+      const sortString = 'dateLastEdited-desc';
+      const result = service.sort(cardDetails2, sortString);
+      const cardDetailsCopy = [...cardDetailsSortedByDate];
+      expect(result).toEqual(cardDetailsCopy.reverse());
+    });
+  });
+
+  describe('paginate(): ', () => {
+    it('should return the correct result based on page size for first page', () => {
+      const result = service.paginate(cardDetails1, 3, 0);
+      expect(result).toEqual(cardDetails1.slice(0, 3));
+    });
+
+    it('should return the correct result based on page size and number', () => {
+      const result = service.paginate(cardDetails1, 2, 3);
+      expect(result).toEqual(cardDetails1.slice(4, 6));
+    });
+
+    it('should return the correct result with page size of 6 if it is null', () => {
+      const result = service.paginate(cardDetails1, null, 2);
+      expect(result).toEqual(cardDetails1.slice(6, 7));
+    });
+
+    it('should return the first 6 elements if pageNumber and pageSize is null', () => {
+      const result = service.paginate(cardDetails1, null, null);
+      expect(result).toEqual(cardDetails1.slice(0, 6));
+    });
   });
 });
---
+++ src/app/core/test-data/card-details.data.ts
@@ -0,0 +1,133 @@
+import { CardDetails } from '../models/card-details.model';
+
+export const cardDetails1: CardDetails[] = [
+  {
+    name: 'Human Web Agent',
+    image: 'https://picsum.photos/640/480',
+    description: 'The Lord of the Rings: The Return of the King',
+    dateLastEdited: '2018-04-16T09:57:36.659Z',
+  },
+  {
+    name: 'The Lion King',
+    image: 'https://picsum.photos/640/480',
+    description: 'Reprehenderit eaque voluptas voluptas nihil cupiditate.',
+    dateLastEdited: '2017-12-28T04:21:00.923Z',
+  },
+  {
+    name: 'District Solutions Orchestrator',
+    image: 'https://picsum.photos/640/480',
+    description: 'Quis harum rerum similique at. The Lion King. Et porro.',
+    dateLastEdited: '2018-10-03T18:56:53.492Z',
+  },
+  {
+    name: 'The Lord of the Rings: The Return of the King',
+    image: 'https://picsum.photos/640/480',
+    description: 'Atque mollitia pariatur quia laborum rerum molestias.',
+    dateLastEdited: '2018-08-06T08:27:26.187Z',
+  },
+  {
+    name: 'Lead Identity Director',
+    image: 'https://picsum.photos/640/480',
+    description:
+      'Nam praesentium voluptate enim perspiciatis doloremque et sint ut. Non modi aperiam quia sed quia omnis inventore quaerat. Quasi at doloremque dolorem eos repudiandae. Qui rerum dolor rerum illum vel doloribus nihil.',
+    dateLastEdited: '2018-01-18T03:31:04.896Z',
+  },
+  {
+    name: 'Customer Assurance Analyst',
+    image: 'https://picsum.photos/640/480',
+    description:
+      'Placeat sit aut. Accusamus et possimus. Veniam vitae necessitatibus. Laborum veritatis molestiae sed.',
+    dateLastEdited: '2018-07-15T17:36:05.500Z',
+  },
+  {
+    name: 'Lead Markets Administrator',
+    image: 'https://picsum.photos/640/480',
+    description:
+      'Unde repellat eos nemo est fugit. Aliquid accusamus iure aspernatur commodi alias culpa. Commodi quae vitae consequatur assumenda rem ea. Beatae illo veritatis consequatur esse. Animi est voluptatem. Dicta rerum dolore.',
+    dateLastEdited: '2018-08-09T23:00:05.748Z',
+  },
+];
+
+export const cardDetails2: CardDetails[] = [
+  {
+    name: 'Human Web Agent',
+    image: 'https://picsum.photos/640/480',
+    description: 'The Lord of the Rings: The Return of the King',
+    dateLastEdited: '2016-04-16T09:57:36.659Z',
+  },
+  {
+    name: 'The Lion King',
+    image: 'https://picsum.photos/640/480',
+    description: 'Reprehenderit eaque voluptas voluptas nihil cupiditate.',
+    dateLastEdited: '2017-12-28T04:21:00.923Z',
+  },
+  {
+    name: 'District Solutions Orchestrator',
+    image: 'https://picsum.photos/640/480',
+    description: 'Quis harum rerum similique at. The Lion King. Et porro.',
+    dateLastEdited: '2018-10-03T18:56:53.492Z',
+  },
+  {
+    name: 'Corporate Integration Consultant',
+    image: 'https://picsum.photos/640/480',
+    description:
+      'Quisquam voluptatem nihil et voluptatem temporibus sapiente. Laboriosam adipisci nihil repudiandae ex. Eius eum eaque. Similique sint fugit molestiae soluta dolore nobis aut.',
+    dateLastEdited: '2018-01-24T19:00:10.561Z',
+  },
+];
+
+export const cardDetailsSortedByName: CardDetails[] = [
+  {
+    name: 'Corporate Integration Consultant',
+    image: 'https://picsum.photos/640/480',
+    description:
+      'Quisquam voluptatem nihil et voluptatem temporibus sapiente. Laboriosam adipisci nihil repudiandae ex. Eius eum eaque. Similique sint fugit molestiae soluta dolore nobis aut.',
+    dateLastEdited: '2018-01-24T19:00:10.561Z',
+  },
+  {
+    name: 'District Solutions Orchestrator',
+    image: 'https://picsum.photos/640/480',
+    description: 'Quis harum rerum similique at. The Lion King. Et porro.',
+    dateLastEdited: '2018-10-03T18:56:53.492Z',
+  },
+  {
+    name: 'Human Web Agent',
+    image: 'https://picsum.photos/640/480',
+    description: 'The Lord of the Rings: The Return of the King',
+    dateLastEdited: '2016-04-16T09:57:36.659Z',
+  },
+  {
+    name: 'The Lion King',
+    image: 'https://picsum.photos/640/480',
+    description: 'Reprehenderit eaque voluptas voluptas nihil cupiditate.',
+    dateLastEdited: '2017-12-28T04:21:00.923Z',
+  },
+];
+
+export const cardDetailsSortedByDate: CardDetails[] = [
+  {
+    name: 'Human Web Agent',
+    image: 'https://picsum.photos/640/480',
+    description: 'The Lord of the Rings: The Return of the King',
+    dateLastEdited: '2016-04-16T09:57:36.659Z',
+  },
+  {
+    name: 'The Lion King',
+    image: 'https://picsum.photos/640/480',
+    description: 'Reprehenderit eaque voluptas voluptas nihil cupiditate.',
+    dateLastEdited: '2017-12-28T04:21:00.923Z',
+  },
+  {
+    name: 'Corporate Integration Consultant',
+    image: 'https://picsum.photos/640/480',
+    description:
+      'Quisquam voluptatem nihil et voluptatem temporibus sapiente. Laboriosam adipisci nihil repudiandae ex. Eius eum eaque. Similique sint fugit molestiae soluta dolore nobis aut.',
+    dateLastEdited: '2018-01-24T19:00:10.561Z',
+  },
+  {
+    name: 'District Solutions Orchestrator',
+    image: 'https://picsum.photos/640/480',
+    description: 'Quis harum rerum similique at. The Lion King. Et porro.',
+    dateLastEdited: '2018-10-03T18:56:53.492Z',
+  },
+];
